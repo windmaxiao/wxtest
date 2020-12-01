85436e5b7f292b8e3407a14dfa3b3d11f3fe327c
windmaxiao <windxiao@vip.qq.com>
2020-12-01 21:39:01 +0800

    Initial commit

---
+++ src/main/java/com/wx/wxtest/WxtestApplication.java
@@ -0,0 +1,13 @@
+package com.wx.wxtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wx/wxtest/WxtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.wxtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
